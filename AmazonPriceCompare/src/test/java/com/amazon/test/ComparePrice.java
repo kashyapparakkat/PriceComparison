0e98ef3b8698fd11b5830760a0f38bc8bbbd4973
cibin mathew <ikonland@gmail.com>
2018-01-21 22:54:38 +0530

    added walmart, ebay prices

--- AmazonPriceCompare/src/test/java/com/amazon/test/ComparePrice.java
+++ AmazonPriceCompare/src/test/java/com/amazon/test/ComparePrice.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ComparePrice {
-	
+
     public static void main(String[] args) {
         String expath = "C:\\Users\\cibin\\Downloads\\chromedriver_win32\\chromedriver.exe";
 
@@ -25,25 +25,51 @@ public class ComparePrice {
         WebDriver driver = new ChromeDriver(options);
 
         System.out.println("test");
-        driver.get("http:\\www.amazon.com");
+
         driver.get("https://www.amazon.com/dp/B00Q31K53M/ref=sspa_dk_detail_4?psc=1");
         try {
-            Thread.sleep(9000);
+            Thread.sleep(6000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        driver.switchTo().frame("pb-iframe");
-        ((ChromeDriver) driver).findElementById("comparePricesBtn").click();System.out.println("test");
-        List<WebElement> priceBlinkList = driver.findElements(By.cssSelector("#compare-prices-content > ul > li > a"));
+//        driver.switchTo().frame("pb-iframe");
+//        ((ChromeDriver) driver).findElementById("comparePricesBtn").click();System.out.println("test");
+        Integer ebay_price = 0;
+        Integer walmart_price = 0;
+        List<WebElement> iframes = driver.findElements(By.tagName("iframe"));
+        if (iframes.size() == 0) {
+            // No frames
+            System.out.println("No priceblink available");
+        } else {
 
-        for (WebElement option : priceBlinkList) {
-            System.out.println("Text :" + option.getAttribute("href"));
-            driver.get(((WebElement) priceBlinkList.get(1)).getAttribute("href"));
-            System.out.println(((ChromeDriver)driver).getCurrentUrl());
-            if (((ChromeDriver)driver).getCurrentUrl().startsWith("https://www.ebay.com")) {
-                System.out.println(priceBlinkList.size());
+
+            Integer amazon_price = Integer.parseInt(((ChromeDriver) driver).findElement(By.cssSelector("#priceblock_ourprice")).getText().replaceAll("[$,]", ""));
+            List<WebElement> priceBlinkList = driver.findElements(By.cssSelector("#compare-prices-content > ul > li > a"));
+            for (WebElement option : priceBlinkList) {
+                System.out.println("Text :" + option.getAttribute("href"));
+                driver.get(((WebElement) priceBlinkList.get(1)).getAttribute("href"));
+                System.out.println(((ChromeDriver) driver).getCurrentUrl());
+                List<String> pricelist = new ArrayList<String>();
+                if (((ChromeDriver) driver).getCurrentUrl().startsWith("https://www.ebay.com")) {
+//            pricelist.add(
+                    ebay_price = Integer.parseInt(driver.findElements(By.cssSelector("#prcIsum")).toString().replaceAll("[$,]", ""));
+                } else if (((ChromeDriver) driver).getCurrentUrl().startsWith("https://www.walmart.com")) {
+                    walmart_price = Integer.parseInt(driver.findElements(By.cssSelector("body > div.js-content > div > div > div > div > div.atf-content > div > div > div:nth-child(2) > div > div.ResponsiveContainer.prod-ProductPage.prod-DefaultLayout.display-flex-ie-compat.direction-flex-column.width-full > div.prod-AboveTheFoldSection.direction-flex-column-m.display-flex-ie-compat > div.prod-rightContainer.prod-MarginTop--xs.display-flex-ie-compat.direction-flex-column > div:nth-child(1) > div.prod-Bot.prod-PositionedRelative > div > div.prod-BotRow.prod-showBottomBorder.prod-OfferSection.prod-OfferSection-twoPriceDisplay > div > div > div:nth-child(1) > div > span > div > span > span > span")).get(0).getText().replaceAll("[$,]", ""));
+                }
+                driver.navigate().back();
+                System.out.println("asdf");
+
             }
-        }
-    }
+            System.out.println(amazon_price);
+            System.out.println(ebay_price);
+            System.out.println(walmart_price);
 
+            if(ebay_price<0.9*amazon_price)
+                System.out.println("ebay is cheaper");
+            if(walmart_price<0.9*amazon_price)
+                System.out.println("walmart is cheaper");
+
+        }
+
+    }
 }
